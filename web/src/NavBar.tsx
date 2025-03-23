8665f260076a4d922c21db3490e3b671f78f6fe7
draihan <draihan@student.ubc.ca>
2025-03-23 11:43:22 -0700

    saa

--- web/src/NavBar.tsx
+++ web/src/NavBar.tsx
@@ -6,7 +6,7 @@ export default function NavBar() {
       <header className="navbar bg-primary text-primary-content fixed top-0 left-0 right-0 mx-auto shadow-lg rounded-2xl w-11/12 mt-4 z-10">
         <div className="flex-1 flex items-center ml-4">
           <Image src="/favicon.ico" alt="Logo" width={40} height={40} className="mr-2" />
-          <a className="btn btn-ghost normal-case text-2xl px-2">Gas</a>
+          <a className="btn btn-ghost normal-case text-2xl px-2">Mangify</a>
         </div>
         <div className="flex-none">
           <nav>
