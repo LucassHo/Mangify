e0208eb9c1b90f28ffd4d2f0db36057cbc3c057e
draihan <draihan@student.ubc.ca>
2025-03-23 01:50:37 -0700

    fix config

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,5 +1,15 @@
 import OpenAI from "openai";
+import type { NextConfig } from "next";
 
 const openaiApiKey = process.env.OPENAI_API_KEY;
 if (!openaiApiKey) throw new Error("OPENAI_API_KEY env not found");
 const openai = new OpenAI({ apiKey: openaiApiKey });
+const nextConfig: NextConfig = {
+  /* config options here */
+  env: {
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+};
+
+export default nextConfig
